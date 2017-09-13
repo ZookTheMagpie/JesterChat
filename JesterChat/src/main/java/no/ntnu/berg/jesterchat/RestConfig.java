261d27ddeded401ff2d2d83eedcfa18f5f832793
Alexander <zookthemagpie@hotmail.com>
2017-09-13 14:03:16 +0200

    ForumService, RestConfig, Conversation, and Message classes added and code copied

--- JesterChat/src/main/java/no/ntnu/berg/jesterchat/RestConfig.java
+++ JesterChat/src/main/java/no/ntnu/berg/jesterchat/RestConfig.java
@@ -1,15 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package no.ntnu.berg.jesterchat;
 
+import java.util.HashSet;
+import java.util.Set;
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import org.glassfish.jersey.media.multipart.MultiPartFeature;
+
 /**
- *
+ * 
  * @author Alexander Eilert Berg
  */
-public class RestConfig
+@ApplicationPath("api")
+public class RestConfig extends Application
 {
+    @Override
+    public Set<Class<?>> getClasses()
+    {
+        Set<Class<?>> resources = new HashSet<>();
+        resources.add(MultiPartFeature.class);
+        addRestResourceClasses (resources);
+        return resources;
+    }
     
+    private void addRestResourceClasses(Set<Class<?>> resources) 
+    {
+        resources.add(no.ntnu.berg.jesterchat.ForumService.class);
+    }
 }
--- JesterChat/src/main/java/no/ntnu/berg/jesterchat/domain/Conversation.java
+++ JesterChat/src/main/java/no/ntnu/berg/jesterchat/domain/Conversation.java
@@ -1,15 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package no.ntnu.berg.jesterchat.domain;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Version;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 
 /**
  *
  * @author Alexander Eilert Berg
  */
-public class Conversation
+@Data @NoArgsConstructor
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@Entity
+public class Conversation implements Serializable
 {
+    @Id
+    String id;
     
+    @XmlTransient
+    @OneToMany(mappedBy = "conversation", cascade = CascadeType.ALL,fetch = FetchType.LAZY)
+    List<Message> messages;
+    
+    @Version
+    Timestamp version;
+    
+    public Conversation(String id)
+    {
+        this.id = id;
+    }
 }
